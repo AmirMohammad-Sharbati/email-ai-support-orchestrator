017fa6841d55478e161656b93dfbdfe3a645afe3
AmirMohammad <amirmohammad.shn84@gmail.com>
2026-06-05 12:02:12 +0330

    refactor: replace hardcoded log level with .env variable

--- src/infrastructure/logger.py
+++ src/infrastructure/logger.py
@@ -1,9 +1,10 @@
 from loguru import logger
+from config.settings import settings
 import sys
 
 def setup_logger():
     logger.remove()
-    logger.add(sys.stdout, level="INFO", format="<green>{time}</green> | <level>{level}</level> | <cyan>{name}</cyan> | {message}")
+    logger.add(sys.stdout, level=settings.log_level, format="<green>{time}</green> | <level>{level}</level> | <cyan>{name}</cyan> | {message}")
     return logger
 
 logger = setup_logger()
